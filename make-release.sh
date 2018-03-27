eecb6bffad8e42a714c81669c4913ccdaef945ca
Allis Tauri <allista@gmail.com>
2018-03-28 00:45:15 +0300

    Added *.pdb to excludes

--- make-release.sh
+++ make-release.sh
@@ -3,6 +3,6 @@
 cd $(dirname "$0")
 
 ../../PyKSPutils/make_mod_release \
--e '*/config.xml' '*.user' '*.orig' '*.mdb' '*.tmp' \
+-e '*/config.xml' '*.user' '*.orig' '*.mdb' '*.pdb' '*.tmp' \
 -i 'saves'
 
