3677f2a230771f267f157c466698775efb97a9fa
Allis Tauri <allista@gmail.com>
2022-02-18 18:38:47 +0300

    Fixed common warnings in make-release.sh
    Also auto-formatted it with PyCharm formatter

--- make-release.sh
+++ make-release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd $(dirname "$0")
+CWD=$(dirname "$0")
+cd "${CWD}" || exit
 
 make_mod_release \
--e '*/config.xml' '*.user' '*.orig' '*.mdb' '*.pdb' '*.tmp' \
--i 'saves'
-
+    -e '*/config.xml' '*.user' '*.orig' '*.mdb' '*.pdb' '*.tmp' \
+    -i 'saves'
